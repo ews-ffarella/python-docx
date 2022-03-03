46bab5ce20531c49e8e38abdd839a2681fdfb28c
ryanamannion <ram321@georgetown.edu>
2022-03-03 17:28:01 -0500

    pep8

--- docx/oxml/text/run.py
+++ docx/oxml/text/run.py
@@ -38,6 +38,7 @@ class CT_R(BaseOxmlElement):
     def _insert_rPr(self, rPr):
         self.insert(0, rPr)
         return rPr
+
     def add_dt(self, text):
         """
         Return a newly added ``<w:delText>`` element containing *text*.
