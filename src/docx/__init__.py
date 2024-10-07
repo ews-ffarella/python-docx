988388e6113c3a103c95b4b3828572599699d87c
ryanamannion <ram321@georgetown.edu>
2024-10-07 16:04:04 -0400

    DEV-4268: commit for 4268-rc0

--- src/docx/__init__.py
+++ src/docx/__init__.py
@@ -13,7 +13,8 @@ from docx.api import Document
 if TYPE_CHECKING:
     from docx.opc.part import Part
 
-__version__ = "1.0.0"
+# __version__ = "1.0.1-a0"
+__version__ = "4268-rc0"
 
 
 __all__ = ["Document"]
